94f69d1ec7cc9472c48bc4ae3dcfd7acc6360e9b
abhishek1561 <ap2043504@gmail.com>
2024-01-24 10:57:00 +0530

    first commit

---
+++ manu.php
@@ -0,0 +1,209 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NavBar</title>
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-sm bg-success style">
+        <div class="container">
+            <a class="navbar-brand" href="#"><img src="images/21.png" alt=""></a>
+            <button class="navbar-toggler bg-secondary" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav w-100 justify-content-end mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link text-white active" aria-current="page" href="#"><i class="fa-solid fa-house text-white"></i>&nbsp;Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white active" aria-current="page" href="#"><i class="fa-solid text-white fa-address-card"></i>&nbsp;Abou Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white active" aria-current="page" href="#"><i class="fa-solid fa-images text-white"></i>&nbsp;Gallery</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white active" aria-current="page" href="#"><i class="fa-solid fa-address-card text-white"></i>&nbsp;Contact Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white active" aria-current="page" href="#"><i class="fa-solid fa-blog text-white"></i>&nbsp;Blog</a>
+                    </li>
+                </ul>
+
+            </div>
+        </div>
+    </nav>
+<!------------------------------------------------------------>
+
+
+    <!---------------------------------------Card Section Start----------------------------------------------->
+    <section>
+        <div class="container py-4">
+            <div class="row">
+                <div class="col-lg-4 col-md-6 col-sm-12">
+                    <div class="card mb-4">
+                        <img src="images/1.jpg" class="card-img-top img-thumbnail" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-12">
+                    <div class="card mb-4">
+                        <img src="images/3.jpg" class="card-img-top img-thumbnail" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-12">
+                    <div class="card mb-4">
+                        <img src="images/2.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+      <!---------------------------------------Card Section End----------------------------------------------->
+
+       <!--------------------------About Us Section Start------------------------------------->
+
+       <section class="about pt-5 pb-5">
+        <div class="container">
+          <div class="row">
+            <h2 class="text-center">About the Company</h2>
+            <p class="text-center mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, nam, ducimus, voluptas explicabo quo iusto deleniti nihil doloribus dolorum quos eius voluptatum voluptate. Hic repellendus distinctio quaerat architecto, ipsam nam.</p>
+            <div class="col-lg-6 col-md-6 about-image" data-aos="fade-in">
+                <img src="images/5.jpg" alt="about image" class="img-fluid border border-success p-4">
+    
+            </div>
+            <div class="col-lg-6 col-md-6 p-4"data-aos="fade-left">
+                <h2 class="text-info">Techsima</h2>
+                <h4 class="text-secondary">Code Your UpTo the Peak.</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum facere aliquam asperiores officia sint perspiciatis id, itaque odit blanditiis omnis et voluptatem accusamus pariatur consequuntur optio excepturi. Odit, quam error.
+                Quo, voluptate? Quo iure expedita quas sit eveniet temporibus soluta officia animi eum inventore tenetur fugit veniam culpa at quidem eos optio itaque, voluptate amet! Non sapiente doloremque debitis beatae.
+                Aspernatur facilis in labore eveniet suscipit, minus incidunt, maiores explicabo, minima laudantium exercitationem. Incidunt, at? Repellendus dolorem quae exercitationem quisquam rem similique enim molestiae dignissimos illum, minima, consequatur illo accusamus.
+                Eveniet ut animi dolorum minima sunt eaque rem veniam minus consequatur rerum quos officia porro enim, debitis sint id inventore praesentium molestiae dolor eligendi nobis. Id voluptatem nam pariatur dolor?</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!------------------------------About Us Section End------------------------------------>
+      <!------------------------Servieces Section Start-------------------------------->
+      
+      <div class="container">
+        <div class="row">
+            <h2 class="text-center">About the Company</h2>
+            <p class="text-center mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, nam, ducimus, voluptas explicabo quo iusto deleniti nihil doloribus dolorum quos eius voluptatum voluptate. Hic repellendus distinctio quaerat architecto, ipsam nam.</p>
+           
+        <div class="col-lg-3 col-md-4 col-sm-6 service-image"data-aos="fade-in-down"><img src="images/9.jpg" alt="service-image" class="img-thumbnail border border-success"></div>
+        <div class="col-lg-3 col-md-4 col-sm-6 service-image"data-aos="zoom-in-up"><img src="images/9.jpg" alt="service-image" class="img-thumbnail border border-success"></div>
+        <div class="col-lg-3 col-md-4 col-sm-6 service-image"data-aos="zoom-in-left"><img src="images/9.jpg" alt="service-image" class="img-thumbnail border border-success"></div>
+        <div class="col-lg-3 col-md-4 col-sm-6 service-image"data-aos="zoom-in-right"><img src="images/9.jpg" alt="service-image" class="img-thumbnail border border-success"></div>
+      </div>
+      </div>
+      <!------------------------Servieces Section End-------------------------------->
+      <!-- --------------------------Enquiry Section Start---------------------------------- -->
+      <section>
+        <div class="container pt-5">
+            <div class="row">
+                <h2 class="text-center p-3">Enquiry Form</h2>
+                <p class="text-center pb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi atque fugit ex veritatis consectetur totam, a placeat tempora in quasi vero voluptas eius similique reprehenderit nobis culpa dignissimos error exercitationem eaque. Sapiente magni ex quos deleniti architecto et soluta. Necessitatibus quo accusamus tenetur ut ipsum libero fugiat sit veniam omnis.</p>
+                <div class="col-lg-6 enquiry-image p-4">
+                    <img src="images/enquiry-img1.jpg" class="img-fluid" alt="enquiry-image">
+                </div>
+                <div class="col-lg-6 enquiry-form p-4">
+                    <div class="form-group">
+                        <label for="name">Name</label>
+                        <input type="text" class="form-control" id="name" placeholder="Enter Your name">
+                        <small id="emailHelp" class="form-text text-mute">We'll never share your email</small>
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="text" class="form-control"id="email" placeholder="Enter Your Email">
+                    </div>
+                    <div class="form-group">
+                        <label for="number">Number</label>
+                        <input type="number" class="form-control"id="number" placeholder="Enter Your Number">
+                    </div>
+                    <div class="form-group">
+                        
+                        <label for="email">Message</label>
+                        <textarea type="text" class="form-control"id="msg" rows="3" placeholder="Enter Your Message.."></textarea>
+                    </div>
+                    <div class="form-group"></div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </div>
+        </div>
+      </section>
+      <!-- --------------------------Enquiry Section End---------------------------------- -->
+      <!-- ----------------------------Footer Section Start----------------------------- -->
+      <footer class="bg-info">
+        <div class="container pt-4">
+            <div class="row">
+                <div class="col-lg-4 footer-content pt-3">
+                    <h1 class="text-center">Techsima</h1>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat quod quo dolorem amet rerum quas vero et molestias debitis quibusdam assumenda eaque, ad atque placeat illo eum vitae est voluptatum! Dignissimos consequuntur possimus distinctio. Velit vitae non dicta alias vero, fugiat doloremque suscipit ad aliquid rerum repudiandae cumque iste, est inventore nobis saepe sint at quas laborum, culpa odit laudantium quidem sequi? Odit, exercitationem quisquam! Id, magni facilis. Possimus, recusandae!</p>
+
+                </div>
+                <div class="col-lg-4 footer-content pt-3 ">
+                    <h3 class="text-center">Social Links</h3>
+                    <p class="p-5 pt-3"><a href=""><i class="fa-brands fa-facebook"></i>&nbsp;&nbsp;Facebook</a></p>
+                    <p class="p-5 pt-0"><a href=""><i class="fa-brands fa-instagram"></i>&nbsp;&nbsp;Instagram</a></p>
+                    <p class="p-5 pt-0"><a href=""><i class="fa-brands fa-linkedin"></i>&nbsp;&nbsp;LinkedIn</a></p>
+                    <p class="p-5 pt-0"><a href=""><i class="fa-brands fa-twitter"></i>&nbsp;&nbsp;Twitter</a></p>
+                </div>
+                <div class="col-lg-4 footer-content pt-3">
+                    <h3 class="text-center">Contect Info</h3>
+                    <p class="p-5 pt-3"><i class="fa-solid fa-location-dot"></i>&nbsp;&nbsp;Save your contacts’ birthdays to see them on your birthday calendar and get helpful cues here and across Google services</p>
+                    <p class="p-5 pt-0"><i class="fa-solid fa-phone"></i>&nbsp;&nbsp;<a href="">+91 7800996687</a></p>
+                    <p class="p-5 pt-0"><i class="fa-solid fa-envelope"></i>&nbsp;&nbsp;<a href="">abhishek123@gmail.com</a></p>
+                </div>
+            </div>
+        </div>
+    </footer>
+      <!-- ----------------------------Footer Section End----------------------------- -->
+      <!-- ---------------------------------Footer line Start---------------------------------------- -->
+      <div class="footer-line bg-info">
+        <div class="container p-0">
+            <div class="hr bg-secondary"></div>
+            <div class="copy-right">
+                <p class="text-center pt-3">@ Techsima solution Private Limited 2023</p>
+            </div>
+        </div>
+      </div>
+      <!-- ---------------------------------Footer line End---------------------------------------- -->
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/all.min.js"></script>
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script>
+      AOS.init({
+        offset: 200,
+        duration: 1000, 
+      });
+    </script>
+</body>
+
+</html>
